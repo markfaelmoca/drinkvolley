271221f5486948873b4c455a4391f90069c03f4b
q1p0w2o9e3i8 <markfaelmoca@gmail.com>
2022-12-03 03:37:51 +0800

    theme dv

---
+++ theme-dv.js
@@ -0,0 +1,14 @@
+/* 
+ alert('Website Maintenance')
+  var password = prompt("Enter in the password");
+        if (password=="q1p0w2o9e3i8") {
+            
+          
+        }
+        else 
+        {
+
+         alert('Password Incorrect Please Come Back Later')
+          location.href = "https://www.google.com";
+        }
+*/
