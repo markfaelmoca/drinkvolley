057d1add87ef1e53ffd9f8ba0116ee66d2c74e38
q1p0w2o9e3i8 <markfaelmoca@gmail.com>
2022-12-07 13:39:15 +0800

    theme-dv

--- theme-dv.js
+++ theme-dv.js
@@ -19,7 +19,3 @@ setTimeout( function ( ) {
 
 
 
-
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
-{js=d.createElement(s);js.id=id;js.src="//portal.referralcandy.com/assets/widgets/refcandy-
-poprocks.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","refcandy-poprocks-js");
