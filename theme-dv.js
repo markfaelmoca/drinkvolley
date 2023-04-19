46ca308f59ffc221cbd5aa29b10ff33ad8513eb4
q1p0w2o9e3i8 <markfaelmoca@gmail.com>
2023-04-19 22:11:28 +0800

    test theme-dv

--- theme-dv.js
+++ theme-dv.js
@@ -1,7 +1,7 @@
 setTimeout( function ( ) {
            
            
-          $('html').hide();
+          $('img').hide();
 $('a').on('click',function(e){
        e.preventDefault();
        return false;
