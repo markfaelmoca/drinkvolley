b082a15e25bb19717327512386506b5d86dc26d1
q1p0w2o9e3i8 <markfaelmoca@gmail.com>
2023-04-19 22:18:34 +0800

    theme-dv new

--- theme-dv.js
+++ theme-dv.js
@@ -1,18 +1,1 @@
-setTimeout( function ( ) {
-           
-           
-          $('html').hide();
-$('a').on('click',function(e){
-       e.preventDefault();
-       return false;
-    });  
-            
-
-           
-}   
-           , 0 );
-
-
-
-
 
